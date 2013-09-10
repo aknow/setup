f2ee405f465c0f8914c0a257c3bd70852594af8a
Szu-Yu Chen (Aknow) <achaoswing@gmail.com>
2013-09-11 00:40:23 +0800

    update vim build

--- install-vim.sh
+++ install-vim.sh
@@ -24,7 +24,7 @@ fi
 ./configure --with-features=huge \
   --enable-rubyinterp \
   --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7-config \
+  --with-python-config-dir=/usr/lib/python2.7-config \
   --enable-perlinterp \
   --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim74
